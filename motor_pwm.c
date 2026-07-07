1da1dfce7cc08e510fa24117fce290261218253c
Isaac Leone <isaac.leone98@gmail.com>
2026-07-06 21:12:27 -0300

    Create motor_pwm.c

---
+++ motor_pwm.c
@@ -0,0 +1,26 @@
+#include "motors_pwm.h"
+#include <webots/robot.h>
+#include <webots/motor.h>
+static WbDeviceTag left_motor;
+static WbDeviceTag right_motor;
+
+void motors_init(void) {
+    left_motor = wb_robot_get_device("left wheel motor");
+    right_motor = wb_robot_get_device("right wheel motor");
+
+    wb_motor_set_position(left_motor, INFINITY);
+    wb_motor_set_position(right_motor, INFINITY);
+
+    motors_stop();
+}
+
+void motors_set_speed(double left_speed, double right_speed) {
+
+    wb_motor_set_velocity(left_motor, left_speed);
+    wb_motor_set_velocity(right_motor, right_speed);
+}
+
+void motors_stop(void) {
+    wb_motor_set_velocity(left_motor, 0.0);
+    wb_motor_set_velocity(right_motor, 0.0);
+}
