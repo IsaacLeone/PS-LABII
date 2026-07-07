1265a6766d5cd7aa563d113d80cdc3b5fd484624
Isaac Leone <isaac.leone98@gmail.com>
2026-07-07 11:32:53 -0300

    Set de controle de odometria e path following (sem controle ainda)

--- meu_controlador.c
+++ meu_controlador.c
@@ -1,17 +1,39 @@
 #include <webots/robot.h>
-#include "motors_pwm.h"
-
+#include "motors_pwm.c"
+#include "robot_control.c"
+#include "path_following.c" 
 
 #define TIME_STEP 64
+#define WHEEL_RADIUS 0.0985   
+#define WHEEL_BASE 0.4044    
 
 int main(int argc, char **argv) {
-
     wb_robot_init();
+    
+    // Inicialização dos módulos do robô
     motors_init();
-    while (wb_robot_step(TIME_STEP) != -1) {       
-        motors_set_speed(2.0, -2.0);
-    }
-    wb_robot_cleanup();
+    control_init(TIME_STEP);
+    path_init();
 
+    while (wb_robot_step(TIME_STEP) != -1) {
+
+        control_update_odometry(TIME_STEP);
+        Pose pose_atual = control_get_pose();
+
+        double v_cmd = 0.0, w_cmd = 0.0;
+        path_compute_control(pose_atual, &v_cmd, &w_cmd);
+
+        double speed_left = (v_cmd - (w_cmd * WHEEL_BASE / 2.0)) / WHEEL_RADIUS;
+        double speed_right = (v_cmd + (w_cmd * WHEEL_BASE / 2.0)) / WHEEL_RADIUS;
+
+        motors_set_speed(speed_left, speed_right);
+        
+        if (path_is_finished()) {
+            motors_stop();
+            break; 
+        }
+    }
+
+    wb_robot_cleanup();
     return 0;
 }
